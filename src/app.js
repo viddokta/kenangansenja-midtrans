3d0d829f637653026d66b5222806662bb4629d02
David Dwi Okta <davidokta56@gmail.com>
2023-12-16 16:40:25 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         formData.append('items', JSON.stringify(items));
 
         try {
-            const response = await fetch('./SSP/placeOrder.php', {
+            const response = await fetch('../php/placeOrder.php', {
                 method: 'POST',
                 body: formData,
             });
